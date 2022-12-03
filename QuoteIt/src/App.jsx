a57022150fc085e2cb4936122554986d6d21ae41
Aditya <aditya.pandey.241103@gmail.com>
2022-12-03 20:39:08 +0530

    Home Page Complete

--- QuoteIt/src/App.jsx
+++ QuoteIt/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Home from './components/Home';
+import Characters from './components/Characters';
 import Navbar from './components/Navbar';
 
-const App = () =>  (
-    <div className='bg-[#0d1117] text-[#ffffff]'>
-      <Navbar/>
-      <Home/>
-    </div>
-    
-  );
+const App = () => (
+  <div className='bg-[#0d1117] text-[#ffffff]'>
+    <Navbar />
+
+    <Home />
+    {/* <Characters/> */}
+  </div>
+
+);
 
 export default App
